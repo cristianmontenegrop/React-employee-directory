48b9d02b6ef60eed21d5df94d6057214c1d1aa7a
Cris M <cristian.montenegro.p@gmail.com>
2020-07-21 17:43:40 -0700

    SearchResults finished

--- employee-directory/src/components/SearchResults/index.js
+++ employee-directory/src/components/SearchResults/index.js
@@ -2,11 +2,34 @@ import React from "react";
 import "./style.css";
 
 function SearchResults(props) {
+
   return (
     <ul className="list-group search-results">
       {props.directory.map(result => (
         <li key={result.email} className="list-group-item">
-          <img alt="Employee" src={result.picture.medium} className="img-fluid" />
+          <div className="container mw-100">
+            <hr className="mt-2 mb-3" />
+            <div className="row">
+
+              {/* <div className="col-2"> */}
+              <img alt="Employee" src={result.picture.large} className="img-fluid" />
+              {/* </div> */}
+              <div className="text-center col-2">
+                {result.name.title} {result.name.first} {result.name.last}
+              </div>
+              <div className="text-center col-2">
+                {result.phone}
+              </div>
+              <div className="text-center col-4">
+                {result.email}
+              </div>
+              <div className="text-center col-2">
+                {result.dob.date.slice(0, 10)}
+              </div>
+            </div>
+            <hr className="mt-2 mb-3" />
+          </div>
+
         </li>
       ))}
     </ul>
